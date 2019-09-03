aa4e7567a068f1598ac3e312d4b8626035224791
qitas <qitas@qitas.cn>
2019-09-03 17:40:17 +0800

    add src

--- examples/Sweep/Sweep.ino
+++ examples/Sweep/Sweep.ino
@@ -10,16 +10,28 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos < 180; pos += 3) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos < 180; pos += 5) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(150);                       // waits 15ms for the servo to reach the position
-    if(pos == 90)delay(1000); 
+    delay(1000);                       // waits 15ms for the servo to reach the position
+    //if(pos == 90)delay(1000); 
   }
-  delay(100);
+  delay(1000);
   for (pos = 180; pos > 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(40);                       // waits 15ms for the servo to reach the position
+    if(pos == 90)delay(1000); 
+  }
+  delay(1000);
+  for (pos = 0; pos > 180; pos += 1) { 
+    myservo.write(pos);              
+    delay(10);   
+    if(pos == 90)delay(100);                     
+ }
+  delay(1000);
+  for (pos = 180; pos > 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(20);                       // waits 15ms for the servo to reach the position
     if(pos == 90)delay(1000); 
   }
 }
